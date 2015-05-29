ee2d67aaf034096df12426a721797e170329689a
Kevin Cotrone <kevincotrone@gmail.com>
2015-05-29 16:36:36 -0500

    Attempting without profiles

--- publish.sh
+++ publish.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 DEFAULT="default"
-PROFILE=${AWS_PROFILE:-$DEFAULT}
 BUCKET=company-blog
 DIR=_site/
 aws configure set aws_access_key_id $AWSKEY
 aws configure set aws_secret_access_key $AWSSECRETKEY
 aws configure set default.output json
-aws  s3  sync $DIR s3://$BUCKET/ --profile "$PROFILE"
+aws  s3  sync $DIR s3://$BUCKET/
