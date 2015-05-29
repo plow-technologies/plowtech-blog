661421a4bc16bcce304b24fe77afa3731352ab18
Kevin Cotrone <kevincotrone@gmail.com>
2015-05-29 16:44:33 -0500

    Using the correct names helps

--- publish.sh
+++ publish.sh
@@ -3,8 +3,5 @@ DEFAULT="default"
 BUCKET=company-blog
 DIR=_site/
 export AWS_ACCESS_KEY_ID=$AWSKEY
-export aws_secret_access_key=$AWSSECRETKEY
-aws configure set aws_access_key_id $AWSKEY
-aws configure set aws_secret_access_key $AWSSECRETKEY
-aws configure set default.output json
+export AWS_SECRET_ACCESS_KEY=$AWSSECRETKEY
 aws  s3  sync $DIR s3://$BUCKET/
