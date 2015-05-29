a38489b52513486b884a385322752aa40105d596
Kevin Cotrone <kevincotrone@gmail.com>
2015-05-29 16:30:17 -0500

    Setting aws keys

--- publish.sh
+++ publish.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
-DEFAULT="blog"
+DEFAULT="default"
 PROFILE=${AWS_PROFILE:-$DEFAULT}
 BUCKET=company-blog
 DIR=_site/
+aws configure set aws_access_key_id $AWSKEY
+aws configure set aws_secret_access_key $AWSSECRETKEY
+aws configure set default.output json
 aws  s3  sync $DIR s3://$BUCKET/ --profile "$PROFILE"
