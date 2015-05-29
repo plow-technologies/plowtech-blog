32c24f1e6c6825625a6ac9c901826871001cbe01
Kevin Cotrone <kevincotrone@gmail.com>
2015-05-29 16:42:25 -0500

    Attempting with environment variables

--- publish.sh
+++ publish.sh
@@ -2,6 +2,8 @@
 DEFAULT="default"
 BUCKET=company-blog
 DIR=_site/
+export AWS_ACCESS_KEY_ID=$AWSKEY
+export aws_secret_access_key=$AWSSECRETKEY
 aws configure set aws_access_key_id $AWSKEY
 aws configure set aws_secret_access_key $AWSSECRETKEY
 aws configure set default.output json
