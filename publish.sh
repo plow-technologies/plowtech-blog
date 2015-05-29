0466fb760d63ac5c9e9223c4da6ca1a7d48811a9
Kevin Cotrone <kevincotrone@gmail.com>
2015-05-29 16:55:15 -0500

    Trying without environment variables

--- publish.sh
+++ publish.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-DEFAULT="default"
 BUCKET=company-blog
 DIR=_site/
-export AWS_ACCESS_KEY_ID=$AWSKEY
-export AWS_SECRET_ACCESS_KEY=$AWSSECRETKEY
 aws  s3  sync $DIR s3://$BUCKET/
